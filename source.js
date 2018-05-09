bece0765eef4cd58cbdfab0e0821805cbcf647a9
baconhair <38001046+baconhair@users.noreply.github.com>
2018-05-09 14:31:01 +0800

    Add files via upload

---
+++ source.js
@@ -0,0 +1,10 @@
+function runCode() {
+
+for (i = 0; i < 1000; i++) {
+    alert("YOUR MOM GAY XD");
+    alert("THIS IS AN ENDLESS LOOP");
+    alert("U CANT ESCAPE")
+    alert("THIS IS GONNA REPEAT ALL OVER AGAIN")
+    alert("HA HA HA")
+}
+}
